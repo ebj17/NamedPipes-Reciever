b3896c3f463af640853510c040af861884483897
ebj17 <126863752+ebj17@users.noreply.github.com>
2024-04-22 14:00:10 +0200

    Initial Commit

---
+++ Reciever.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+class PipeReciever
+{
+    static void Main(string[] message)
+    {
+        using var client = new NamedPipeClientStream ("BigPipe");
+        client.Connect();
+
+        using var reader = new StreamReader(client);
+        Console.WriteLine(reader.ReadLine());
+    }
+}
